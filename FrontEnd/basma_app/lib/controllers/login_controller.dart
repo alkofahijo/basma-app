a6c5193564dd6c64faf05accf36b86c25a60aecb
Ismaeel Khalil Abuzaydeh <abuzaydeh60@gmail.com>
2025-11-12 00:51:25 +0300

    re design the login page and write it using Getx

---
+++ FrontEnd/basma_app/lib/controllers/login_controller.dart
@@ -0,0 +1,31 @@
+import 'package:basma_app/pages/report/select_location_page.dart';
+import 'package:get/get.dart';
+import '../../services/api_service.dart';
+
+class LoginController extends GetxController {
+  var email = ''.obs;
+  var password = ''.obs;
+  var isLoading = false.obs;
+  var obscurePassword = true.obs;
+  var errorMessage = ''.obs;
+
+  Future<void> login() async {
+    if (email.isEmpty || password.isEmpty) {
+      errorMessage.value = 'Please Fill All Fields';
+      return;
+    }
+    try {
+      isLoading.value = true;
+      await ApiService.login(email.value.trim(), password.value);
+      Get.to(() => const SelectLocationPage());
+    } catch (e) {
+      errorMessage.value = 'Login Failed. Please Try Again.';
+    } finally {
+      isLoading.value = false;
+    }
+  }
+
+  void togglePassword() {
+    obscurePassword.value = !obscurePassword.value;
+  }
+}
