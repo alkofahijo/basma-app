8ae188ef58c347a281c54b048c3e98d31c83b6b5
Ismaeel Khalil Abuzaydeh <abuzaydeh60@gmail.com>
2025-11-12 00:50:00 +0300

    Change MaterialApp to GetMaterial App

--- FrontEnd/basma_app/lib/main.dart
+++ FrontEnd/basma_app/lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:basma_app/pages/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    GetMaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(scaffoldBackgroundColor: const Color(0xFFEFF1F1)),
       home: SplashScreen(),
