df69056b4ee2b0c7ca229d6dfb0e43725707bdac
Ismaeel Khalil Abuzaydeh <abuzaydeh60@gmail.com>
2025-11-11 18:46:24 +0300

    Spalsh Screen

---
+++ FrontEnd/basma_app/lib/pages/splash_screen.dart
@@ -0,0 +1,30 @@
+import 'package:basma_app/pages/on_boarding_screen.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 5), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const OnBoardingScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color(0xFFEFF1F1),
+      body: Center(child: Image(image: AssetImage("assets/images/splash.png"))),
+    );
+  }
+}
